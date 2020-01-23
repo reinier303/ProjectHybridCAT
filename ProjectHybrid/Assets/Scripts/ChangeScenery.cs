2548905e6b1a6d8dbe4beb9ba4f2ffcfd233c119
Bas <Bas.ghosts@gmail.com>
2020-01-23 16:43:58 +0100

    Trying fixing trying

---
+++ ProjectHybrid/Assets/Scripts/ChangeScenery.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScenery : MonoBehaviour
+{
+    [SerializeField]
+    public Transform LeftController;
+    [SerializeField]
+    public Transform RightController;
+
+    private List<GameObject> sceneryObjects = new List<GameObject>();
+    private List<GameObject> randomScenery = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach(GameObject foundObject in GameObject.FindGameObjectsWithTag("Scenery"))
+        {
+            if(!foundObject.GetComponent<Scenery>())
+            {
+                foundObject.AddComponent(typeof(Scenery));
+            }
+            foundObject.GetComponent<Scenery>().controllerL = LeftController;
+            foundObject.GetComponent<Scenery>().controllerR = RightController;
+            sceneryObjects.Add(foundObject);
+        }
+        GetRandomScenery();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        foreach (GameObject sObject in sceneryObjects)
+        {
+            Scenery sceneryScript = sObject.GetComponent<Scenery>();
+            sceneryScript.ChangeMaterialColor(true);
+        }
+        //float vertical = Input.GetAxis("Vertical");
+
+        /*
+        else
+        {
+            foreach (GameObject sObject in sceneryObjects)
+            {
+                Scenery sceneryScript = sObject.GetComponent<Scenery>();
+                sceneryScript.ChangeMaterialColor(false);
+            }
+        }*/
+
+    }
+
+    private void GetRandomScenery()
+    {
+        randomScenery.Clear();
+        Debug.Log(sceneryObjects.Count);
+        foreach (GameObject scenery in sceneryObjects)
+        {
+            if(Random.Range(0, 101) < Random.Range(60, 90))
+            {
+                randomScenery.Add(scenery);
+            }
+        }
+        Debug.Log(randomScenery.Count);
+    }
+}
--- ProjectHybrid/Assets/Scripts/Scenery.cs
+++ ProjectHybrid/Assets/Scripts/Scenery.cs
@@ -10,8 +10,15 @@ public class Scenery : MonoBehaviour
     [SerializeField]
     private float speedFactor = 0.003f;
     [SerializeField]
-    private Transform controllerR, controllerL;
-    
+    public Transform controllerR, controllerL;
+
+    public Vector2 RZAxis = new Vector2(0, 2);
+
+    //Start     -0.52
+
+    //POS -0.71
+
+    //NEG - 0.16
 
     private void Start()
     {
@@ -26,7 +33,7 @@ public class Scenery : MonoBehaviour
         float currentMetallic = meshRenderer.material.GetFloat("_Metallic");
         float currentSmoothness = meshRenderer.material.GetFloat("_Glossiness");
 
-
+        
         float randomValueR = Random.Range(-0.015f, 0.015f);
         float randomValueG = Random.Range(-0.015f, 0.015f);
         float randomValueB = Random.Range(-0.015f, 0.015f);
@@ -44,14 +51,15 @@ public class Scenery : MonoBehaviour
         */
         //Change color depending on where in the process we are.
 
-        currentValue = controllerR.localPosition.z;
+        currentValue = controllerR.localPosition.y;
+        Debug.Log(currentValue);
         //Backwards: Change value according to difference from start to current value.
         
         float differenceSteps;
 
-        if (!positive && currentValue > 0)
+        if (currentValue > (RZAxis.x + RZAxis.y) / 2)
         {
-            differenceSteps = currentValue / speedFactor;
+            differenceSteps = currentValue;
             meshRenderer.material.color = new Color(currentColor.r + (StartColor.r - currentColor.r) / differenceSteps,
                                                     currentColor.g + (StartColor.g - currentColor.g) / differenceSteps,
                                                     currentColor.b + (StartColor.b - currentColor.b) / differenceSteps, currentColor.a);
@@ -59,9 +67,9 @@ public class Scenery : MonoBehaviour
             meshRenderer.material.SetFloat("_Glossiness", currentMetallic + (0 - currentMetallic) / differenceSteps);
 
         }
-        if (positive && currentValue < 0)
+        if (currentValue < (RZAxis.x + RZAxis.y) / 2)
         {
-            differenceSteps = currentValue / -speedFactor;
+            differenceSteps = currentValue;
             meshRenderer.material.color = new Color(currentColor.r + (StartColor.r - currentColor.r) / differenceSteps,
                                                     currentColor.g + (StartColor.g - currentColor.g) / differenceSteps,
                                                     currentColor.b + (StartColor.b - currentColor.b) / differenceSteps, currentColor.a);
@@ -70,19 +78,18 @@ public class Scenery : MonoBehaviour
         }
 
         //Forwards: Change value at random.
-        if (positive && currentValue < 1 && currentValue > 0)
+        if (currentValue < RZAxis.x && currentValue > (RZAxis.x + RZAxis.y) / 2)
         {
             meshRenderer.material.color = new Color(currentColor.r + randomValueR, currentColor.g + randomValueG, currentColor.b + randomValueB, currentColor.a);
             meshRenderer.material.SetFloat("_Metallic", currentMetallic + Random.Range(0.0015f, 0.0045f));
             meshRenderer.material.SetFloat("_Glossiness", currentMetallic + Random.Range(0.001f, 0.0025f));
         }
-        if (!positive && currentValue > -1 && currentValue < 0)
+        if (currentValue > RZAxis.y && currentValue < (RZAxis.x + RZAxis.y) / 2)
         {
             meshRenderer.material.color = new Color(currentColor.r + randomValueR, currentColor.g + randomValueG, currentColor.b + randomValueB, currentColor.a);
             meshRenderer.material.SetFloat("_Metallic", currentMetallic + Random.Range(0.0015f, 0.0045f));
             meshRenderer.material.SetFloat("_Glossiness", currentMetallic + Random.Range(0.001f, 0.0025f));
         }
         print(currentValue);
-
     }
 }
