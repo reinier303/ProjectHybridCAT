343f742db9d78a45b059e125958a4dd1e93a862c
Bas <Bas.ghosts@gmail.com>
2020-01-23 15:06:52 +0100

    Distortian shader / triyp shit yo

---
+++ ProjectHybrid/Assets/_Scripts/CameraTripEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTripEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //VignetteAndChromaticAberration chromatic_Vignette = Camera.main.GetComponent<VignetteAndChromaticAberration>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //.chromaticAberration = Mathf.PingPong(Time.time, 2));
+    }
+}
