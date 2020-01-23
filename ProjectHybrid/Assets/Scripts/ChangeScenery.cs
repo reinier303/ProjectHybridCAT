c2d97f53fa7bda5ea34b375d6232e12b05a21991
reinier303 <30725987+reinier303@users.noreply.github.com>
2020-01-23 11:21:24 +0100

    Added lotsa things

---
+++ ProjectHybrid/Assets/Scripts/ChangeScenery.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScenery : MonoBehaviour
+{
+    private List<GameObject> sceneryObjects = new List<GameObject>();
+    private List<GameObject> randomScenery = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach(GameObject foundObject in GameObject.FindGameObjectsWithTag("Scenery"))
+        {
+            sceneryObjects.Add(foundObject);
+        }
+        GetRandomScenery();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        if(vertical > 0)
+        {
+            foreach (GameObject sObject in randomScenery)
+            {
+                Scenery sceneryScript = sObject.GetComponent<Scenery>();
+                sceneryScript.ChangeMaterialColor(true);
+            }
+        }
+        if (vertical < 0)
+        {
+            foreach (GameObject sObject in randomScenery)
+            {
+                Scenery sceneryScript = sObject.GetComponent<Scenery>();
+                sceneryScript.ChangeMaterialColor(false);
+            }
+        }
+
+    }
+
+    private void GetRandomScenery()
+    {
+        randomScenery.Clear();
+        Debug.Log(sceneryObjects.Count);
+        foreach (GameObject scenery in sceneryObjects)
+        {
+            if(Random.Range(0, 101) < Random.Range(60, 90))
+            {
+                randomScenery.Add(scenery);
+            }
+        }
+        Debug.Log(randomScenery.Count);
+    }
+}
---
+++ ProjectHybrid/Assets/Scripts/Scenery.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenery : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    private Color StartColor;
+    private float currentValue;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        StartColor = meshRenderer.material.color;
+        currentValue = 0;
+    }
+
+    public void ChangeMaterialColor(bool positive)
+    {
+        Color currentColor = meshRenderer.material.color;
+        float randomValueR = Random.Range(-0.015f, 0.015f);
+        float randomValueG = Random.Range(-0.015f, 0.015f);
+        float randomValueB = Random.Range(-0.015f, 0.015f);
+
+        //Change currentValue to know where in the process we are.
+        if(positive && currentValue < 1)
+        {
+            currentValue += 0.002f;
+        }
+        if (!positive && currentValue > -1)
+        {
+            currentValue -= 0.002f;
+        }
+
+        //Change color depending on where in the process we are.
+
+
+        //Backwards: Change value according to difference from start to current value.
+        
+        float differenceSteps;
+
+        if (!positive && currentValue > 0)
+        {
+            differenceSteps = currentValue / 0.002f;
+            meshRenderer.material.color = new Color(currentColor.r + (StartColor.r - currentColor.r) / differenceSteps,
+                                                    currentColor.g + (StartColor.g - currentColor.g) / differenceSteps,
+                                                    currentColor.b + (StartColor.b - currentColor.b) / differenceSteps, currentColor.a);
+        }
+        if(positive && currentValue < 0)
+        {
+            differenceSteps = currentValue / -0.002f;
+            meshRenderer.material.color = new Color(currentColor.r + (StartColor.r - currentColor.r) / differenceSteps,
+                                                    currentColor.g + (StartColor.g - currentColor.g) / differenceSteps,
+                                                    currentColor.b + (StartColor.b - currentColor.b) / differenceSteps, currentColor.a);
+        }
+
+        //Forwards: Change value at random.
+        if (positive && currentValue < 1 && currentValue > 0)
+        {
+            meshRenderer.material.color = new Color(currentColor.r + randomValueR, currentColor.g + randomValueG, currentColor.b + randomValueB, currentColor.a);
+        }
+        if (!positive && currentValue > -1 && currentValue < 0)
+        {
+            meshRenderer.material.color = new Color(currentColor.r + randomValueR, currentColor.g + randomValueG, currentColor.b + randomValueB, currentColor.a);
+        }
+        print(currentValue);
+
+    }
+}
