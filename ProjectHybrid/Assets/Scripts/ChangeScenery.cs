7957d3e2b3abbbf678b63119743421230b11d90f
reinier303 <30725987+reinier303@users.noreply.github.com>
2020-01-28 11:57:15 +0100

    Added All axisses

--- ProjectHybrid/Assets/Scripts/ChangeScenery.cs
+++ ProjectHybrid/Assets/Scripts/ChangeScenery.cs
@@ -11,11 +11,14 @@ public class ChangeScenery : MonoBehaviour
 
     public List<GameObject> sceneryObjects = new List<GameObject>();
     public List<GameObject> lights = new List<GameObject>();
+    public List<GameObject> Audio = new List<GameObject>();
     private List<GameObject> randomScenery = new List<GameObject>();
 
     public float RangeFactorX;
     public float RangeFactorY;
     public float RangeFactorZ;
+
+    public AudioSource Left, Right;
 
     public static ChangeScenery Instance;
 
@@ -50,9 +53,33 @@ public class ChangeScenery : MonoBehaviour
             Scenery sceneryScript = foundObject.GetComponent<Scenery>();
             sceneryScript.controllerL = LeftController;
             sceneryScript.controllerR = RightController;
+            sceneryScript.RangeFactorX = RangeFactorX;
+            sceneryScript.RangeFactorY = RangeFactorY;
             sceneryScript.RangeFactorZ = RangeFactorZ;
 
             lights.Add(foundObject);
+
+            sceneryScript.Initialize();
+
+        }
+
+        foreach (GameObject foundObject in GameObject.FindGameObjectsWithTag("Audio"))
+        {
+            if (!foundObject.GetComponent<Scenery>())
+            {
+                foundObject.AddComponent(typeof(Scenery));
+            }
+            Scenery sceneryScript = foundObject.GetComponent<Scenery>();
+            sceneryScript.controllerL = LeftController;
+            sceneryScript.controllerR = RightController;
+            sceneryScript.RangeFactorX = RangeFactorX;
+            sceneryScript.RangeFactorY = RangeFactorY;
+            sceneryScript.RangeFactorZ = RangeFactorZ;
+
+            sceneryScript.Left = Left;
+            sceneryScript.Right = Right;
+
+            Audio.Add(foundObject);
 
             sceneryScript.Initialize();
 
@@ -75,18 +102,14 @@ public class ChangeScenery : MonoBehaviour
             Scenery sceneryScript = light.GetComponent<Scenery>();
             sceneryScript.ChangeLights();
         }
-        //float vertical = Input.GetAxis("Vertical");
 
-        /*
-        else
+        foreach (GameObject audio in Audio)
         {
-            foreach (GameObject sObject in sceneryObjects)
-            {
-                Scenery sceneryScript = sObject.GetComponent<Scenery>();
-                sceneryScript.ChangeMaterialColor(false);
-            }
-        }*/
+            Scenery sceneryScript = audio.GetComponent<Scenery>();
+            sceneryScript.ChangeSoundLX();
+            sceneryScript.ChangeSoundRZ();
 
+        }
     }
 
     private void GetRandomScenery()
--- ProjectHybrid/Assets/Scripts/Scenery.cs
+++ ProjectHybrid/Assets/Scripts/Scenery.cs
@@ -11,8 +11,15 @@ public class Scenery : MonoBehaviour
     private float speedFactor = 0.003f;
     [SerializeField]
     public Transform controllerR, controllerL;
+    public AudioSource Left, Right;
+
 
     public Vector2 RZAxis = new Vector2(0, 2);
+
+    //Sound
+    private float startPitchL, startPitchR;
+    private float startWetmix, startChorus;
+
 
     //Lights
     private float startIntensity;
@@ -25,13 +32,21 @@ public class Scenery : MonoBehaviour
     private Vector2 startTiling;
 
     //Axis Info
-    private float currentValueX;
-    private float currentValueY;
-    private float currentValueZ;
+    private float RcurrentValueX;
+    private float RcurrentValueY;
+    private float RcurrentValueZ;
+
+    private float LcurrentValueX;
+    private float LcurrentValueY;
+    private float LcurrentValueZ;
 
     public float RStartValueX;
     public float RStartValueY;
     public float RStartValueZ;
+
+    public float LStartValueX;
+    public float LStartValueY;
+    public float LStartValueZ;
 
     public float RangeFactorX;
     public float RangeFactorY;
@@ -49,7 +64,16 @@ public class Scenery : MonoBehaviour
     {
         meshRenderer = GetComponent<MeshRenderer>();
         light = GetComponent<Light>();
-        if(light != null)
+        if(Left != null && Right != null)
+        {
+            Left.outputAudioMixerGroup.audioMixer.GetFloat("FlangeRate", out startPitchL);
+            Right.outputAudioMixerGroup.audioMixer.GetFloat("Distortion", out startPitchR);
+            Right.outputAudioMixerGroup.audioMixer.GetFloat("Wetmix", out startWetmix);
+            Left.outputAudioMixerGroup.audioMixer.GetFloat("ChorusDepth", out startChorus);
+
+        }
+
+        if (light != null)
         {
             startIntensity = light.intensity;
         }
@@ -73,8 +97,13 @@ public class Scenery : MonoBehaviour
         RStartValueX = controllerR.localPosition.x / RangeFactorX;
         RStartValueY = controllerR.localPosition.y / RangeFactorY;
         RStartValueZ = controllerR.localPosition.z / RangeFactorZ;
-    }
 
+        LStartValueX = controllerL.localPosition.x / RangeFactorX;
+        LStartValueY = controllerL.localPosition.y / RangeFactorY;
+        LStartValueZ = controllerL.localPosition.z / RangeFactorZ;
+    }
+    
+    //Right Y Axis
     public void ChangeMaterialColorController()
     {
         Color currentColor = meshRenderer.material.color;
@@ -85,44 +114,77 @@ public class Scenery : MonoBehaviour
         float randomValueG = Random.Range(-0.015f, 0.015f);
         float randomValueB = Random.Range(-0.015f, 0.015f);
 
-        currentValueY = controllerR.localPosition.y / RangeFactorY;
+        RcurrentValueY = controllerR.localPosition.y / RangeFactorY;
 
         if(RGorB == 0)
         {
-            meshRenderer.material.color = new Color(startColor.r + (currentValueY - RStartValueY), startColor.g, startColor.b, currentColor.a);
+            meshRenderer.material.color = new Color(startColor.r + (RcurrentValueY - RStartValueY), startColor.g, startColor.b, currentColor.a);
         }
         else if (RGorB == 1)
         {
-            meshRenderer.material.color = new Color(startColor.r, startColor.g + (currentValueY - RStartValueY), startColor.b, currentColor.a);
+            meshRenderer.material.color = new Color(startColor.r, startColor.g + (RcurrentValueY - RStartValueY), startColor.b, currentColor.a);
         }
         else if(RGorB == 2)
         {
-            meshRenderer.material.color = new Color(startColor.r, startColor.g, startColor.b + (currentValueY - RStartValueY), currentColor.a);
+            meshRenderer.material.color = new Color(startColor.r, startColor.g, startColor.b + (RcurrentValueY - RStartValueY), currentColor.a);
         }
 
         meshRenderer.material.SetFloat("_Metallic", currentMetallic + (0 - currentMetallic));
         meshRenderer.material.SetFloat("_Glossiness", currentMetallic + (0 - currentMetallic));
     }
 
+    //Left Y Axis
     public void ChangeMateriaTiling()
     {
         float currentTilingX = meshRenderer.material.mainTextureScale.x;
         float currentTilingY = meshRenderer.material.mainTextureScale.y;
         Vector2 currentTiling = new Vector2(currentTilingX, currentTilingY);
 
-        currentValueX = controllerR.localPosition.x / RangeFactorX;
+        LcurrentValueY = controllerL.localPosition.y / RangeFactorY;
 
-        meshRenderer.material.mainTextureScale = new Vector2(startTiling.x + (currentValueX - RStartValueX), startTiling.y + (currentValueX - RStartValueX));
+        meshRenderer.material.mainTextureScale = new Vector2(startTiling.y + (RcurrentValueY - LStartValueY), startTiling.y + (RcurrentValueY - LStartValueY));
     }
 
+    //Right X Axis
     public void ChangeLights()
     {
-
         float currentIntensity = light.intensity;
 
-        currentValueZ = controllerR.localPosition.z / RangeFactorZ;
+        RcurrentValueX = controllerR.localPosition.x / RangeFactorX;
 
-        light.intensity = startIntensity + (currentValueZ - RStartValueZ);
+        light.intensity = startIntensity + (RcurrentValueX - RStartValueX);
+    }
+
+    //Left X Axis
+    public void ChangeSoundLX()
+    {
+        float currentPitchL = Left.pitch;
+        float currentPitchR = Right.pitch;
+
+        LcurrentValueX = controllerL.localPosition.x / RangeFactorX;
+
+        Left.outputAudioMixerGroup.audioMixer.SetFloat("FlangeRate", startPitchL + (LcurrentValueX - LStartValueX));
+        Left.outputAudioMixerGroup.audioMixer.SetFloat("FlangeDepth", startPitchL + (LcurrentValueX - LStartValueX));
+
+        float currentDistortion;
+        Right.outputAudioMixerGroup.audioMixer.GetFloat("Distortion", out currentDistortion);
+        Right.outputAudioMixerGroup.audioMixer.SetFloat("Distortion", startPitchR - (LcurrentValueX - LStartValueX));
+        //Right.pitch = startPitchR - ((LcurrentValueX - LStartValueX));
+
+    }
+
+    //Right Z Axis
+    public void ChangeSoundRZ()
+    {
+        RcurrentValueZ = controllerR.localPosition.z / RangeFactorZ;
+        
+        print(startWetmix);
+        Right.outputAudioMixerGroup.audioMixer.SetFloat("Wetmix", startWetmix + (RcurrentValueZ - RStartValueZ));
+        Left.outputAudioMixerGroup.audioMixer.SetFloat("ChorusDepth", startChorus - (RcurrentValueZ - RStartValueZ));
+
+
+        float currentWetmix;
+        Right.outputAudioMixerGroup.audioMixer.GetFloat("Wetmix", out currentWetmix);
 
     }
 
