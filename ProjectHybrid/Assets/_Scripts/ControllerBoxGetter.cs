0663d0261564faf8a3e6927297eb85e424f2a057
Bas <Bas.ghosts@gmail.com>
2020-01-28 13:40:36 +0100

    Boxes on Controllers / marchins / Camera Rig Prefab fixed

---
+++ ProjectHybrid/Assets/_Scripts/ControllerBoxGetter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerBoxGetter : MonoBehaviour
+{
+    public GameObject ControllerBox;
+    public GameObject ControllerBox2;
+    public GameObject[] ControllerModels;
+
+    private void Start()
+    {
+        ControllerModels = GameObject.FindGameObjectsWithTag("Controller");
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var obj in ControllerModels)
+        {
+            if (obj.transform.childCount > 0) continue;
+            if (Vector3.Distance(ControllerBox.transform.position, obj.transform.position) < 2f)
+            {
+                ControllerBox.transform.parent = obj.transform;
+                ControllerBox.transform.position = new Vector3(0, 0, 0);
+            }
+            if(Vector3.Distance(ControllerBox2.transform.position, obj.transform.position) < 2f)
+            {
+                ControllerBox2.transform.parent = obj.transform;
+                ControllerBox2.transform.position = new Vector3(0, 0, 0);
+            }
+        }
+     }
+}
